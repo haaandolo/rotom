c6a0729d597ac524f1fd0ee1dd580bc2393b31bd
haaandolo <76086118+haaandolo@users.noreply.github.com>
2024-07-03 22:16:26 +1200

    feat: add inital test for subscribe func

--- src/data/subscriber/mod.rs
+++ src/data/subscriber/mod.rs
@@ -37,9 +37,6 @@ where
             IntoIterator<Item = (ExchangeConnector, &'static str, &'static str, StreamType)>,
         ExchangeConnector: Connector + Eq + std::hash::Hash + std::fmt::Debug,
     {
-        // exchange_sub is a hashmap of where the key is a exchange connector and
-        // value is a vec of ExchangeSub structs. Here we are essentially grouping
-        // by the exchange connector and geting the subscriptions associated with it
         let mut exchange_subscriptions = HashMap::new();
         subscriptions
             .into_iter()
@@ -71,5 +68,109 @@ where
                 (exchange_name, rx)
             })
             .collect::<HashMap<_, _>>()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[derive(PartialEq, Debug)]
+    pub struct Instrument<'a> {
+        pub base: &'a str,
+        pub quote: &'a str,
+        pub stream_type: &'a str,
+    }
+
+    impl<'a> Instrument<'a> {
+        fn new(_base: &'a str, _quote: &'a str, _stream_type: &'a str) -> Self {
+            Self {
+                base: _base,
+                quote: _quote,
+                stream_type: _stream_type,
+            }
+        }
+    }
+    #[derive(Eq, PartialEq, Hash, Debug)]
+    pub struct BinanceSpot;
+
+    impl BinanceSpot {
+        pub fn exchange_id(&self) -> String {
+            String::from("BinanceSpot")
+        }
+    }
+
+    #[derive(PartialEq, Debug)]
+    pub struct Subscription<'a, ExchangeConnector> {
+        pub connector: ExchangeConnector,
+        pub subscriptions: Vec<Instrument<'a>>,
+    }
+
+    impl<'a, ExchangeConnector> Subscription<'a, ExchangeConnector> {
+        fn new(_connector: ExchangeConnector, _subscriptions: Vec<Instrument<'a>>) -> Self {
+            Self {
+                connector: _connector,
+                subscriptions: _subscriptions,
+            }
+        }
+    }
+
+    #[test]
+    fn test_subscribe_function() {
+        // This is just an example but should not mix different stream
+        // types in the same subscription
+        let subscriptions = [
+            (BinanceSpot, "arb", "usdt", "Trades"),
+            (BinanceSpot, "arb", "usdt", "Trades"),
+            (BinanceSpot, "btc", "usdt", "L2"),
+        ];
+
+        // Test first part of function
+        let mut exchange_subscriptions = HashMap::new();
+        subscriptions
+            .into_iter()
+            .collect::<HashSet<_>>() // rm duplicates
+            .into_iter()
+            .for_each(|(exchange_conn, base, quote, stream_type)| {
+                exchange_subscriptions
+                    .entry(exchange_conn)
+                    .or_insert(Vec::new())
+                    .push(Instrument::new(base, quote, stream_type))
+            });
+
+        let mut expected_subscriptions = HashMap::new();
+        expected_subscriptions.insert(
+            BinanceSpot,
+            vec![
+                Instrument::new("arb", "usdt", "Trades"),
+                Instrument::new("btc", "usdt", "L2"),
+            ],
+        );
+
+        assert_eq!(exchange_subscriptions, exchange_subscriptions);
+
+        // Test second part of function
+        let mut market_subscriptions = HashMap::new();
+        exchange_subscriptions
+            .into_iter()
+            .for_each(|(exchange_connector, instruments)| {
+                let exchange_id = exchange_connector.exchange_id();
+                let subscriptions = Subscription::new(exchange_connector, instruments);
+                market_subscriptions.insert(exchange_id, subscriptions);
+            });
+
+        let mut expected_market_subscriptions = HashMap::new();
+        expected_market_subscriptions.insert(
+            "BinanceSpot".to_string(),
+            Subscription::new(
+                BinanceSpot,
+                vec![
+                    Instrument::new("arb", "usdt", "Trades"),
+                    Instrument::new("btc", "usdt", "L2"),
+                ],
+            ),
+        );
+
+        assert_eq!(market_subscriptions, expected_market_subscriptions);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,40 @@ async fn main() {
     //////////////////
     // Build Streams
     let mut streams = StreamBuilder::new()
-        // .subscribe([
-        //     (BinanceSpot, "arb", "usdt", StreamType::Trades),
-        //     (BinanceSpot, "arb", "usdt", StreamType::Trades),
-        //     (BinanceSpot, "btc", "usdt", StreamType::L2),
-        // ])
         .subscribe([
-            // (PoloniexSpot, "arb", "usdt", StreamType::Trades),
-            // (PoloniexSpot, "arb", "usdt", StreamType::Trades),
-            (PoloniexSpot, "btc", "usdt", StreamType::L2),
-            (PoloniexSpot, "arb", "usdt", StreamType::L2),
+            (BinanceSpot, "arb", "usdt", StreamType::Trades),
+            (BinanceSpot, "arb", "usdt", StreamType::Trades),
+            (BinanceSpot, "btc", "usdt", StreamType::L2),
         ])
+        // .subscribe([
+        //     // (PoloniexSpot, "arb", "usdt", StreamType::Trades),
+        //     // (PoloniexSpot, "arb", "usdt", StreamType::Trades),
+        //     (PoloniexSpot, "btc", "usdt", StreamType::L2),
+        //     (PoloniexSpot, "arb", "usdt", StreamType::L2),
+        // ])
         .init()
         .await;
 
-    // // Read from socket
-    // if let Some(mut receiver) = streams.remove("binancespot") {
-    //     while let Some(msg) = receiver.recv().await {
-    //         // Some(msg);
-    //         println!("----- Binance -----");
-    //         // let msg: BinanceBookUpdate = serde_json::from_str(&msg).unwrap();
-    //         println!("{:#?}", msg);
-    //     }
-    // }
+    // Read from socket
+    if let Some(mut receiver) = streams.remove("binancespot") {
+        while let Some(msg) = receiver.recv().await {
+            // Some(msg);
+            println!("----- Binance -----");
+            println!("{:#?}", msg);
+        }
+    }
 
     // Read from socket
     if let Some(mut receiver) = streams.remove("poloniexspot") {
         while let Some(msg) = receiver.recv().await {
             // Some(msg);
             println!("----- Poloniex -----");
-            // let msg: PoloniexBookUpdate = serde_json::from_str(&msg).unwrap();
             println!("{:#?}", msg);
         }
     }
 }
 
 // todo
-// - empty bid ask vec's should be skipped in deserialization process
-// - write test for the subscribe fn in stream builder
 // - process custom ping for poloniex
 // - add mismatch sequence error in websocket
 // - multi stream builder
