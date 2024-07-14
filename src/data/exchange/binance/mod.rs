ed93e18d141d2ab5ef446d7cb9048462f14289d5
haaandolo <76086118+haaandolo@users.noreply.github.com>
2024-07-14 19:40:53 +1200

    change: exchange_connector -> exchange

---
+++ src/data/exchange/binance/mod.rs
@@ -0,0 +1,58 @@
+pub mod book;
+pub mod channel;
+
+use book::{BinanceBook, BinanceSubscriptionResponse};
+use channel::BinanceChannel;
+use serde_json::json;
+use std::collections::HashSet;
+
+use super::{Connector, Instrument, StreamSelector};
+use crate::data::{protocols::ws::ws_client::WsMessage, models::{book::OrderBookL2, subs::{ExchangeId, StreamType}}};
+
+#[derive(Debug, Default, Eq, PartialEq, Hash)]
+pub struct BinanceSpot;
+
+impl Connector for BinanceSpot {
+    type ExchangeId = ExchangeId;
+    type SubscriptionResponse = BinanceSubscriptionResponse;
+
+    const ID: ExchangeId = ExchangeId::BinanceSpot;
+
+    fn url() -> String {
+        BinanceChannel::SPOT_WS_URL.as_ref().to_string()
+    }
+
+    fn requests(subscriptions: &[Instrument]) -> Option<WsMessage> {
+        let channels = subscriptions
+            .iter()
+            .map(|s| {
+                let stream = match s.stream_type {
+                    StreamType::L1 => BinanceChannel::ORDER_BOOK_L1.as_ref(),
+                    StreamType::L2 => BinanceChannel::ORDER_BOOK_L2.as_ref(),
+                    StreamType::Trades => BinanceChannel::TRADES.as_ref(),
+                };
+                format!("{}{}{}", s.base, s.quote, stream).to_lowercase()
+            })
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let binance_request = json!({
+            "method": "SUBSCRIBE",
+            "params": channels,
+            "id": 1
+        });
+
+        Some(WsMessage::Text(binance_request.to_string()))
+    }
+
+    fn validate_subscription(subscription_response: String, _subscriptions: &[Instrument]) -> bool {
+        let subscription_response =
+            serde_json::from_str::<BinanceSubscriptionResponse>(&subscription_response).unwrap();
+        subscription_response.result.is_none()
+    }
+}
+
+impl StreamSelector<BinanceSpot, OrderBookL2> for BinanceSpot {
+    type Stream = BinanceBook;
+}
---
+++ src/data/exchange/poloniex/mod.rs
@@ -0,0 +1,71 @@
+pub mod book;
+pub mod channel;
+
+use book::{PoloniexBook, PoloniexSubscriptionResponse};
+use channel::PoloniexChannel;
+use serde_json::json;
+use std::collections::HashSet;
+
+use super::{Connector, Instrument, StreamSelector};
+use crate::data::protocols::ws::ws_client::{PingInterval, WsMessage};
+use crate::data::models::book::OrderBookL2;
+use crate::data::models::subs::{ExchangeId, StreamType};
+
+#[derive(Debug, Default, Eq, PartialEq, Hash)]
+pub struct PoloniexSpot;
+
+impl Connector for PoloniexSpot {
+    type ExchangeId = ExchangeId;
+    type SubscriptionResponse = PoloniexSubscriptionResponse;
+
+    const ID: ExchangeId = ExchangeId::PoloniexSpot;
+
+    fn url() -> String {
+        PoloniexChannel::SPOT_WS_URL.as_ref().to_string()
+    }
+
+    fn requests(sub: &[Instrument]) -> Option<WsMessage> {
+        let channels = sub
+            .iter()
+            .map(|s| match s.stream_type {
+                StreamType::L2 => PoloniexChannel::ORDER_BOOK_L2.as_ref(),
+                StreamType::Trades => PoloniexChannel::TRADES.as_ref(),
+                _ => panic!("Invalid stream was enter for poloniex"),
+            })
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let tickers = sub
+            .iter()
+            .map(|s| format!("{}_{}", s.base, s.quote))
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let poloniex_sub = json!({
+            "event": "subscribe",
+            "channel": channels,
+            "symbols": tickers
+        });
+
+        Some(WsMessage::text(poloniex_sub.to_string()))
+    }
+
+    fn ping_interval() -> Option<PingInterval> {
+        Some(PingInterval {
+            time: 30,
+            message: json!({"event": "ping"}),
+        })
+    }
+
+    fn validate_subscription(subscription_response: String, sub: &[Instrument]) -> bool {
+        let subscription_response =
+            serde_json::from_str::<PoloniexSubscriptionResponse>(&subscription_response).unwrap();
+        subscription_response.symbols.len() == sub.len()
+    }
+}
+
+impl StreamSelector<PoloniexSpot, OrderBookL2> for PoloniexSpot {
+    type Stream = PoloniexBook;
+}
